7ad76acab12f8ac29a41d201a820a8df8ac1cd79
kelsnki15 <kelsnki15@gmail.com>
2021-07-15 16:07:13 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+function consultaCep(){
+    var cep = document.getElementById("cep").value;
+    var url =  "https://viacep.com.br/ws/"+cep+"/json/";    
+    console.log(url);
+    
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $("#titulo_cep").html("CEP " + response.cep);
+            $(".cep").show();
+
+            //document.getElementById("cep").innerHTML = response.cep;
+           // document.getElementById("logradouro").innerHTML = response.logradouro;
+            //document.getElementById("bairro").innerHTML = response.bairro;
+            //document.getElementById("localidade").innerHTML = response.localidade;
+           
+        }
+    })
+}
+
+$(function(){
+    $(".cep").hide();
+});
